143b5b3f97c31e75beaa4ae5982165b3b7a080b2
yuliia <yuliia@internationalshowtimes.com>
2026-01-09 15:47:54 +0200

    Add human choice logic

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function getComputerChoice() {
-    choices = ["rock", "paper", "scissors"];
-    choice = Math.round(Math.random() * (choices.length - 1));
+    let choices = ["rock", "paper", "scissors"];
+    let choice = Math.round(Math.random() * (choices.length - 1));
     return choices[choice];
+}
+
+function getHumanChoice() {
+    let isItMistake = true;
+
+    while (isItMistake) {
+
+        choice = prompt('Type you weapon: rock, paper or scissors').toLowerCase();
+
+        if (choice == "rock" || choice == "paper" || choice == "scissors") {
+            return choice;
+        }
+        else {
+            console.log("You probably made a typo. Please try again.")
+        }
+    }
+
+
 }
