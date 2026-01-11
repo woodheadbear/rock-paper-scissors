a9fbc8206248f6dccd70cc63eb608c6b81ab96d4
yuliia <yuliia@internationalshowtimes.com>
2026-01-11 20:09:34 +0200

    Change equality to strict for more predictable result

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getHumanChoice() {
 
         let choice = prompt('Type you weapon: rock, paper or scissors').toLowerCase();
 
-        if (choice == "rock" || choice == "paper" || choice == "scissors") {
+        if (choice === "rock" || choice === "paper" || choice === "scissors") {
             return choice;
         }
         else {
@@ -22,13 +22,13 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
+    if (humanChoice === computerChoice) {
         console.log(`Computer chose "${computerChoice}". You chose "${humanChoice}". It's draw! Whatever. 😕`)
     }
     else if (
-        (humanChoice == "rock" && computerChoice == "scissors") ||
-        (humanChoice == "paper" && computerChoice == "rock") ||
-        (humanChoice == "scissors" && computerChoice == "paper")) {
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")) {
         console.log(`Computer chose "${computerChoice}". You chose "${humanChoice}". You win! Sheesh! 😬`);
         humanScore++;
     }
