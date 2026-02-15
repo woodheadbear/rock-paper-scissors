76301283576c8469133a34614f453a62b92dacfc
woodheadbear <213650906+woodheadbear@users.noreply.github.com>
2026-02-15 23:09:56 +0200

    Add game score information to DOM

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const WINPOINTS = 5;
+
 const choices = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
+let round = 0;
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * choices.length);
@@ -18,16 +21,20 @@ function humanWins(humanChoice, computerChoice) {
 function playRound(humanChoice, computerChoice) {
 
     if (humanChoice === computerChoice) {
-        console.log(`Computer chose "${computerChoice}". You chose "${humanChoice}". It's draw! Whatever. 😕`);
-        return "draw";
+        round += 1;
+        return `Computer chose "${computerChoice}". You chose "${humanChoice}". It's draw! Whatever. 😕`;
     }
     else if (humanWins(humanChoice, computerChoice)) {
-        console.log(`Computer chose "${computerChoice}". You chose "${humanChoice}". You win! Sheesh! 😬`);
+        round += 1;
         humanScore += 1;
+        return `Computer chose "${computerChoice}". You chose "${humanChoice}". You win! Sheesh! 😬`;
+
     }
     else {
-        console.log(`Computer chose "${computerChoice}". You chose "${humanChoice}". You lose! Muahahaha! 🤖`);
+        round += 1;
         computerScore += 1;
+        return `Computer chose "${computerChoice}". You chose "${humanChoice}". You lose! Muahahaha! 🤖`;
+
     }
 }
 
@@ -35,15 +42,22 @@ function playGame() {
     const div = document.querySelector('#human-choice');
     div.addEventListener('click', (e) => {
         const button = e.target.closest('button');
+        if (!button) return;
+
+        let info = document.querySelector('#info');
+        let score = document.querySelector('#score');
+
         let humanChoice = button.textContent.toLowerCase();
         let computerChoice = getComputerChoice();
 
-        playRound(humanChoice, computerChoice);
 
-        if (humanScore === 5) {
+        info.textContent = playRound(humanChoice, computerChoice);
+        score.textContent = `Round ${round}: Your score ${humanScore}. Computer score ${computerScore}.`;
+
+        if (humanScore === WINPOINTS) {
             console.log("You are winner, lucky bastard!!! 😔");
         }
-        else if (computerScore === 5) {
+        else if (computerScore === WINPOINTS) {
             console.log("You are loser. Taste my SILICON POWER!!! 😈");
         }
 
