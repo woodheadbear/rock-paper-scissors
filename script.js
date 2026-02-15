53e98ff7abbe9a58373322ab989cfd8359ba547f
woodheadbear <213650906+woodheadbear@users.noreply.github.com>
2026-02-16 01:13:37 +0200

    Change logic from console input to input by ui

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const choices = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
 let round = 0;
+let gameOver = false;
+
+let info = document.querySelector('#info');
+let score = document.querySelector('#score');
+let winner = document.querySelector('#winner');
+const div = document.querySelector('#human-choice');
+let allButtons = div.querySelectorAll('button');
+const restart = document.querySelector('#restart');
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * choices.length);
@@ -18,20 +26,39 @@ function humanWins(humanChoice, computerChoice) {
     );
 }
 
-function playRound(humanChoice, computerChoice) {
+function setButtonsDisabled(status) {
+    for (let i = 0; i < allButtons.length; i++) {
+        allButtons[i].disabled = status;
+    }
+}
 
+function endGame() {
+    setButtonsDisabled(true);
+    gameOver = true;
+}
+
+function resetGame() {
+    setButtonsDisabled(false);
+
+    humanScore = 0;
+    computerScore = 0;
+    round = 0;
+    gameOver = false;
+
+    info.textContent = "";
+    score.textContent = "";
+    winner.textContent = '';
+}
+
+function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        round += 1;
         return `Computer chose "${computerChoice}". You chose "${humanChoice}". It's draw! Whatever. 😕`;
     }
     else if (humanWins(humanChoice, computerChoice)) {
-        round += 1;
         humanScore += 1;
         return `Computer chose "${computerChoice}". You chose "${humanChoice}". You win! Sheesh! 😬`;
-
     }
     else {
-        round += 1;
         computerScore += 1;
         return `Computer chose "${computerChoice}". You chose "${humanChoice}". You lose! Muahahaha! 🤖`;
 
@@ -39,28 +66,30 @@ function playRound(humanChoice, computerChoice) {
 }
 
 function playGame() {
-    const div = document.querySelector('#human-choice');
     div.addEventListener('click', (e) => {
         const button = e.target.closest('button');
         if (!button) return;
-
-        let info = document.querySelector('#info');
-        let score = document.querySelector('#score');
+        if (gameOver) return;
 
         let humanChoice = button.textContent.toLowerCase();
         let computerChoice = getComputerChoice();
 
-
+        round += 1;
         info.textContent = playRound(humanChoice, computerChoice);
         score.textContent = `Round ${round}: Your score ${humanScore}. Computer score ${computerScore}.`;
 
         if (humanScore === WINPOINTS) {
-            console.log("You are winner, lucky bastard!!! 😔");
+            endGame();
+            winner.textContent = "You are winner, lucky bastard!!! 😔";
         }
         else if (computerScore === WINPOINTS) {
-            console.log("You are loser. Taste my SILICON POWER!!! 😈");
+            endGame();
+            winner.textContent = "You are loser. Taste my SILICON POWER!!! 😈";
         }
+    });
 
+    restart.addEventListener('click', () => {
+        resetGame();
     });
 }
 playGame();
